69e7d11e5aa10e08a8dec5972b89bac44f5f396b
emilykimhuynh <emilyhuynh101@gmail.com>
2017-03-04 14:19:34 -0600

    stuff

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function(){
-  return gulp.src('themes/3d/static/scss/*.scss')
+  return gulp.src('themes/3d/static/scss/config.scss')
     .pipe(sass())
     .pipe(gulp.dest('themes/3d/static/css'))
 });
